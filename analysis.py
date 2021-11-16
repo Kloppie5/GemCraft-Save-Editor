7e94fb52f16376ff0452a221a127a0149fd1b5d0
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2021-11-16 15:42:49 +0100

    Decode base64 save

--- analysis.py
+++ analysis.py
@@ -1,10 +1,13 @@
+import base64
 
 def print_save(filepath):
     with open(filepath) as f:
         file_data = f.read()
-    encrypted_save_data = file_data[:-16]
+    save_data_base64 = file_data[:-16]
+    save_data_something = base64.b64decode(save_data_base64.encode('ascii'))
+
     checksum = file_data[-16:]
-    print(f'Data: {encrypted_save_data}\n'
+    print(f'Data: {save_data_something}\n'
         + f'Checksum: {checksum}\n')
 
 print_save('./testing-saves/high-level.dat')
