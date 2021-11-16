686d93791c5df354a50bbbd43fe8af48014fbd43
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2021-11-16 15:34:57 +0100

    Split off presumed checksum

--- analysis.py
+++ analysis.py
@@ -2,7 +2,10 @@
 def print_save(filepath):
     with open(filepath) as f:
         file_data = f.read()
-    print(file_data)
+    encrypted_save_data = file_data[:-16]
+    checksum = file_data[-16:]
+    print(f'Data: {encrypted_save_data}\n'
+        + f'Checksum: {checksum}\n')
 
 print_save('./testing-saves/high-level.dat')
 print_save('./testing-saves/low-level.dat')
